a1ff81932fd83a53d4f45a0cd4a4aa077e486d8a
yuun <1tyuun@gmail.com>
2024-03-26 20:16:42 +0900

    v0.1 bases

---
+++ week04_bases.py
@@ -0,0 +1,7 @@
+char = input() # chr: 정수를 문자열로
+print (ord(char)) #ord: 문자열을 정수로. chr과 역계산 방법이다.
+
+print(0b1010, 0xa, 0o12)
+ten = int(input())
+print(bin(ten), oct(ten), hex(ten), chr(ten)) #chr function return ASCII code.67
+
