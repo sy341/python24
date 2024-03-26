19b4ed8f3632c710ec3923a4344afbfb00b85c0e
yuun <1tyuun@gmail.com>
2024-03-26 21:25:40 +0900

    v0.2 math

--- week04_math.py
+++ week04_math.py
@@ -1,39 +1,19 @@
-def is_prime_number(n) -> bool:
-#is_prime_number = True
- if k < 2:
-    #is_prime_number = False
-    return False
- else:
-    i = 2
-    while i *i <= k:
-        if k % i == 0:
-            #is_prime_number = False
-            #break
-            return False
-        i = i +1
-    return True
+def print_fx(fx):
+    texts = ("f(x) = ")
+    expo = len(fx) - 1
+
+    for i in range(len(fx)):
+        coef = fx[i]
+        if coef == 0:
+            expo = expo - 1
+            continue
+        if coef>= 0 and i != 0:
+            texts = texts + "+"
+        texts = texts + str(coef) + "x^" + str(expo) + " " #str: 정수를 문자열으로.
+        expo = expo - 1
+    return texts
 
 
-def power(b, e) -> int:...\
+coefficient = [5,-2,0,6]
 
-
-while True:
-    menu = int(input("1) prime number  2) power  3) divmod  4) quit : "))
-    if menu == 1:
-        start, end = list(map(int, input("input start & end number : ").split()))
-        for k in range(start, end + 1):
-            if is_prime_number(k): print(k, end=' ')
-    elif menu == 2:
-        base, exponent = map(int, input(" Input base & exponent number : ").split())
-        print(f"{base}^{exponent} = {base ** exponent}")
-    elif menu == 3:
-        dividend, divisor = map(int, input("input dividend & divisor number : ").split())
-        print(f"{dividend} // {divisor} = {divmod(dividend, divisor)[0]}")
-        print(f"{dividend} % {divisor} = {divmod(dividend, divisor)[1]}")
-    elif menu == 4:
-        print ("exit program...")
-        break
-    else:
-        print("please choose from the n=menu.")
-
-
+print(print_fx(coefficient))
