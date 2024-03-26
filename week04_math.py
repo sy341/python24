e4d2d252f8b0d992122267346b13dfd4ae9bc6b1
yuun <1tyuun@gmail.com>
2024-03-26 21:40:14 +0900

    v0.3 math

--- week04_math.py
+++ week04_math.py
@@ -14,6 +14,19 @@ def print_fx(fx):
     return texts
 
 
+def calculate_fx(fx, x): #다항식.. 값을 대입해서.. 계산하는 겁니다..
+    expo = len(fx) -1
+    result = 0
+
+    for k in range(len(fx)):
+        coef = fx[k]
+        result = result + coef * x ** expo
+        expo = expo - 1
+
+    return result
+
 coefficient = [5,-2,0,6]
 
 print(print_fx(coefficient))
+x = int(input("Input x value : ")) #이제 계산하는 함수를 만들어서 적용시켜야 한다.
+print(calculate_fx(coefficient, x))
