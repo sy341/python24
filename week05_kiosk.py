ab2de0af4a7ac0b260fafeca309d1b703640276b
yuun <1tyuun@gmail.com>
2024-04-02 20:40:16 +0900

    v0.3 kiosk

--- week05_kiosk.py
+++ week05_kiosk.py
@@ -1,21 +1,32 @@
 # ISHS Cafe
 # caffe latte: 2500, americano: 1500
 
-beverage = ["Americano", "caffe latte"]
-price = [1500, 2500]
+beverage = ["Americano", "Caffe latte", "Iced tea"]
+price = [1500, 2500, 2300]
 total_price = 0
+quantity = [0, 0, 0]
 
 while True: #주문 종료 시까지 돌리는 게 키오스크.
-    menu = input("1) americano  2) caffe latte  3) end order : ")
-    if menu == '3':
+    menu = input("1) americano  2) caffe latte  3) Iced tea  4) end order : ")
+    if menu == '4':
         print ("Exit program")
         break
+    elif menu == '3':
+        print("You ordered caffe latte. The price is 2300 won.")
+        total_price = total_price + price[2]
+        quantity[2] = quantity[2] + 1
     elif menu == '2':
         print ("You ordered caffe latte. The price is 1500 won.")
-        total_price = total_price + price[0]
+        total_price = total_price + price[1]
+        quantity[1] = quantity[1] + 1
     elif menu == '1':
         print("You ordered americano. The price is 2500 won.")
-        total_price = total_price + price[1]
+        total_price = total_price + price[0]
+        quantity[0] = quantity[0] + 1
     else:
         print(f"Menu number {menu} you ordered does not exist. Please choose from the menu.")
+
+print(f"{beverage[0]} x{quantity[0]}")
+print(f"{beverage[1]} x{quantity[1]}")
+print(f"{beverage[2]} x{quantity[2]}")
 print(f"Your order has veen accepted. The total amount is {total_price} won.")
