1104ed95fca7d013c6c30fdf343c1b71c05d033c
yuun <1tyuun@gmail.com>
2024-05-07 19:41:06 +0900

    v0.2 eg prime number

--- e.g..py
+++ e.g..py
@@ -1,11 +1,14 @@
 number = int(input("Input number : "))
 
+#number: prime number, 1과 자기 자신만을 약수로 가짐.
+#1이 아닌 다른 수를 약수로 가질 수 없음.
 count = 0
-for i in range(1, number+1):
+#for i in range(1, number+1):
+for i in range(2, number+1):
     if number % i == 0:
         count += 1
 
-if count == 2:
+if count == 0:
     print(f"{number} is prime number~")
 else:
     print(f"{number} is NOT prime number!")
